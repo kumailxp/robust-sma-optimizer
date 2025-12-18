3b7ffbb3b6d5e45777ab99cd734eb08de42d055c
Kumail Ahmed <kumail89@gmail.com>
2025-12-18 21:32:54 +0100

    lint code

--- backtest_sma_optimisation.py
+++ backtest_sma_optimisation.py
@@ -2,46 +2,50 @@
 import math
 import os
 from typing import Any
-import yfinance as yf
-import pandas as pd
-import numpy as np
-import matplotlib
-# Set the backend to 'Agg' (A Graphics Group)
-matplotlib.use('Agg')
-import matplotlib.pyplot as plt
-import matplotlib.colors as mcolors
-from concurrent.futures import ThreadPoolExecutor, as_completed
 
+import matplotlib as mpl
+import numpy as np
+import pandas as pd
+import yfinance as yf
+
+# Set the backend to 'Agg' (A Graphics Group)
+mpl.use("Agg")
+import argparse
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from datetime import datetime, timedelta
 from random import randint, randrange
-from datetime import timedelta, datetime
-from backtest_plot_sma_strategy_result import plot_all_strategies
+
+import matplotlib.colors as mcolors
+import matplotlib.pyplot as plt
 import picologging as logging
 from rich.progress import Progress
 from rich_argparse import RichHelpFormatter
-import argparse
+
+from backtest_plot_sma_strategy_result import plot_all_strategies
+
 
 def initialise_logger():
     # 1. Define the Logger
     # Get a logger instance, usually named after the module or 'root'
     logger = logging.getLogger(__name__)
-    logger.setLevel(logging.DEBUG) # Set the minimum level to capture (DEBUG and above)
+    logger.setLevel(logging.DEBUG)  # Set the minimum level to capture (DEBUG and above)
 
     # 2. Define the Formatter (how the log line looks)
     formatter = logging.Formatter(
-        '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-        datefmt='%Y-%m-%d %H:%M:%S'
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
     )
 
     # 3. Define the FileHandler
-    LOG_FILE_NAME = 'app.log'
-    file_handler = logging.FileHandler(LOG_FILE_NAME, mode='w')
-    file_handler.setLevel(logging.DEBUG) # File handler logs DEBUG and above
+    LOG_FILE_NAME = "app.log"
+    file_handler = logging.FileHandler(LOG_FILE_NAME, mode="w")
+    file_handler.setLevel(logging.DEBUG)  # File handler logs DEBUG and above
     file_handler.setFormatter(formatter)
 
     logger.addHandler(file_handler)
 
     stream_handler = logging.StreamHandler()
-    stream_handler.setLevel(logging.CRITICAL) # Console only logs INFO and above
+    stream_handler.setLevel(logging.CRITICAL)  # Console only logs INFO and above
     stream_handler.setFormatter(formatter)
     logger.addHandler(stream_handler)
 
@@ -49,27 +53,25 @@ def initialise_logger():
 
 
 logger = initialise_logger()
+
+
 def read_file_to_list_efficiently(filename):
-    """
-    Reads a file line by line and saves the stripped content to a list.
-    """
+    """Reads a file line by line and saves the stripped content to a list."""
     lines_list = []
     try:
-        with open(filename, 'r') as file:
+        with open(filename) as file:
             for line in file:
                 lines_list.append(line.strip())
-        
+
         return lines_list
-        
+
     except FileNotFoundError:
         logger.info(f"Error: The file '{filename}' was not found.")
         return []
 
 
-
 def random_date(start, end):
-    """
-    This function will return a random datetime between two datetime
+    """This function will return a random datetime between two datetime
     objects.
     """
     delta = end - start
@@ -92,7 +94,7 @@ def create_random_date_ranges(minimum_start_date, ranges_to_create):
             continue
         rand_end_year = randint(int(years_remaining / 2), years_remaining)
         end_date = datetime(
-            year=sd.year + rand_end_year, month=randint(1, 10), day=randint(1, 28)
+            year=sd.year + rand_end_year, month=randint(1, 10), day=randint(1, 28),
         )
         end_date_as_str = end_date.strftime("%Y-%m-%d")
         logger.info(f"start date: {start_date_as_str}, end_date: {end_date_as_str}")
@@ -101,8 +103,7 @@ def create_random_date_ranges(minimum_start_date, ranges_to_create):
 
 
 def group_nearby_values(data_list, max_gap):
-    """
-    Finds groups of nearby values in a list.
+    """Finds groups of nearby values in a list.
 
     Args:
         data_list (list): The list of numerical values.
@@ -111,6 +112,7 @@ def group_nearby_values(data_list, max_gap):
 
     Returns:
         list: A list of lists, where each inner list is a group.
+
     """
     if not data_list:
         return []
@@ -137,13 +139,14 @@ def group_nearby_values(data_list, max_gap):
 
     return groups
 
+
 def most_frequent(List):
     return max(set(List), key=List.count)
 
-def plot_ranges(data_range, max_sma, ticker, folder_name):
 
+def plot_ranges(data_range, max_sma, ticker, folder_name):
     color_names = []
-    for name, _ in mcolors.TABLEAU_COLORS.items():
+    for name in mcolors.TABLEAU_COLORS:
         color_names.append(name)
 
     max_colors_len = len(color_names)
@@ -161,16 +164,16 @@ def plot_ranges(data_range, max_sma, ticker, folder_name):
         plot_best_sma(final_sma, color_names[id % max_colors_len])
 
     for s in final_smas_data:
-        list_of_all_smas.extend([i for i in s.index])
+        list_of_all_smas.extend(list(s.index))
 
     list_of_all_smas = sorted(list_of_all_smas)
     clustered_results = group_nearby_values(list_of_all_smas, 10)
-    clustered_results.sort(key=lambda f : len(f))
+    clustered_results.sort(key=lambda f: len(f))
     best_smas = []
     for idx, g in enumerate(reversed(clustered_results)):
-         if idx == 2:
-             break
-         best_smas.append(most_frequent(g))
+        if idx == 2:
+            break
+        best_smas.append(most_frequent(g))
 
     plt.title(f"Good SMAs for {ticker}")
     plt.ylabel("Final Portfolio Value (USD)")
@@ -179,8 +182,10 @@ def plot_ranges(data_range, max_sma, ticker, folder_name):
     # plt.legend()
     plt.grid(True, which="major", ls="-", alpha=0.3)
     filename = f"best_smas_{ticker}.png"
-    save_path = os.path.join(folder_name, filename) # Joins folder_name and filename reliably
-    plt.savefig(save_path, format='png', dpi=300, bbox_inches='tight')
+    save_path = os.path.join(
+        folder_name, filename,
+    )  # Joins folder_name and filename reliably
+    plt.savefig(save_path, format="png", dpi=300, bbox_inches="tight")
     plt.close()
     return best_smas
 
@@ -204,14 +209,13 @@ def get_best_moving_avgs(results, final_bnh, start_date, end_date):
     logger.info(f"HODL: ${final_bnh:,.2f} from {start_date} to {end_date}")
     for i in range(len(final_smas)):
         logger.info(
-            f"Good SMA: {final_smas.index[i]}, ROI: ${final_smas.iloc[i]:,.2f}, Ratio (roi/hodl): {final_smas.iloc[i]/final_bnh:.2f}"
+            f"Good SMA: {final_smas.index[i]}, ROI: ${final_smas.iloc[i]:,.2f}, Ratio (roi/hodl): {final_smas.iloc[i] / final_bnh:.2f}",
         )
     logger.info("-" * 50)
     return final_smas
 
 
-def plot_best_sma(final_smas, color_name):
-
+def plot_best_sma(final_smas, color_name) -> None:
     for i in range(len(final_smas)):
         plt.plot(
             final_smas.index[i],
@@ -221,8 +225,8 @@ def plot_best_sma(final_smas, color_name):
             markersize=8,
         )
 
-def plot_data(results, final_bnh, color_name):
 
+def plot_data(results, final_bnh, color_name) -> None:
     results_series = pd.Series(results)
 
     transparent_color = list(mcolors.to_rgba(color_name))
@@ -252,7 +256,7 @@ def backtest_sma_optimization(data, start_date, end_date, min_sma, max_sma):
 
     if len(data) == 0:
         logger.info("Error: No data found.")
-        return
+        return None
 
     # Prepare results storage
     results = {}
@@ -273,7 +277,6 @@ def backtest_sma_optimization(data, start_date, end_date, min_sma, max_sma):
 
     # 2. Loop Through All SMA Periods
     for sma_period in range(min_sma, max_sma + 1):
-
         # Calculate SMA on the full 'data' DataFrame (for the full historical period)
         temp_sma = data["Close"].rolling(window=sma_period).mean()
 
@@ -309,7 +312,7 @@ def backtest_sma_optimization(data, start_date, end_date, min_sma, max_sma):
     return (results, final_bnh, start_date, end_date)
 
 
-def plot_all(ticker, folder_name, ranges_to_create, start_date, end_date):
+def plot_all(ticker, folder_name, ranges_to_create, start_date, end_date) -> None:
     min_sma = 1
     max_sma = 200
     data_to_plot = []
@@ -325,48 +328,86 @@ def plot_all(ticker, folder_name, ranges_to_create, start_date, end_date):
         progress=False,
         multi_level_index=False,
         interval="1d",
-        auto_adjust=True
+        auto_adjust=True,
     )
     date_array = create_random_date_ranges(minimum_start_date, ranges_to_create)
     futures = []
-    with ThreadPoolExecutor(max_workers=1) as executor:
-        with Progress() as progress:
-            task1 = progress.add_task(f"Analysing {ticker}", total=ranges_to_create)
-            for s_date, e_date in date_array:
-                future = executor.submit(
-                    backtest_sma_optimization, downloaded_data, s_date, e_date, min_sma, max_sma
-                )
-                futures.append(future)
-            for future in as_completed(futures):
-                try:
-                    data_to_plot.append(future.result())
-                    progress.update(task1, advance=1)
-                except:
-                    pass
+    with ThreadPoolExecutor(max_workers=1) as executor, Progress() as progress:
+        task1 = progress.add_task(f"Analysing {ticker}", total=ranges_to_create)
+        for s_date, e_date in date_array:
+            future = executor.submit(
+                backtest_sma_optimization,
+                downloaded_data,
+                s_date,
+                e_date,
+                min_sma,
+                max_sma,
+            )
+            futures.append(future)
+        for future in as_completed(futures):
+            try:
+                data_to_plot.append(future.result())
+                progress.update(task1, advance=1)
+            except Exception as _:
+                pass
     b_smas = plot_ranges(data_to_plot, max_sma, ticker, f"{folder_name}/best_smas")
     logger.info(f"Best of all SMAs: {b_smas}")
     if len(b_smas) != 0:
-        plot_all_strategies(ticker, start_date, end_date, downloaded_data, 1000, b_smas, f"{folder_name}/results")
+        plot_all_strategies(
+            ticker,
+            start_date,
+            end_date,
+            downloaded_data,
+            1000,
+            b_smas,
+            f"{folder_name}/results",
+        )
+
 
 if __name__ == "__main__":
-
-    description_str =  """
+    description_str = """
     \033[38;5;208mDescription:\033[0m
     Simulate sma strategy and backtest the best smas.
     Training dataset is from 2014-01-01 to 2021-01-01.
-    Default testing dataset is from 2021-01-01 to 2025-06-01, 
+    Default testing dataset is from 2021-01-01 to 2025-06-01,
     but the user can change this using the --start-date and --end-date flags.
     """
-    parser = argparse.ArgumentParser(description=description_str, formatter_class=RichHelpFormatter)
-    parser.add_argument("-t", "--ticker", nargs='+', help="ticker symbol")
-    parser.add_argument("-s", "--number-of-simulations", help="number of simulations to run (default: 50)", type=int, default=50)
-    parser.add_argument("--start-date", help="start date for testing dataset", type=str, default="2021-01-01")
-    parser.add_argument("--end-date", help="end date for testing dataset", type=str, default="2025-06-01")
+    parser = argparse.ArgumentParser(
+        description=description_str, formatter_class=RichHelpFormatter,
+    )
+    parser.add_argument("-t", "--ticker", nargs="+", help="ticker symbol")
+    parser.add_argument(
+        "-s",
+        "--number-of-simulations",
+        help="number of simulations to run (default: 50)",
+        type=int,
+        default=50,
+    )
+    parser.add_argument(
+        "--start-date",
+        help="start date for testing dataset",
+        type=str,
+        default="2021-01-01",
+    )
+    parser.add_argument(
+        "--end-date",
+        help="end date for testing dataset",
+        type=str,
+        default="2025-06-01",
+    )
     folder_name = "my_plots"
     os.makedirs(f"{folder_name}/best_smas", exist_ok=True)
     os.makedirs(f"{folder_name}/results", exist_ok=True)
 
     args = parser.parse_args()
-    tickers = args.ticker if args.ticker else read_file_to_list_efficiently("ticker_list.txt")
+    tickers = (
+        args.ticker if args.ticker else read_file_to_list_efficiently("ticker_list.txt")
+    )
     for ticker in tickers:
-        plot_all(ticker, folder_name, args.number_of_simulations, args.start_date, args.end_date)
+        plot_all(
+            ticker,
+            folder_name,
+            args.number_of_simulations,
+            args.start_date,
+            args.end_date,
+        )
